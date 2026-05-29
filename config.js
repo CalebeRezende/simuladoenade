41e768894c6460b37e4af595d75b6aba57c076d8
CalebeRezende <120114655+CalebeRezende@users.noreply.github.com>
2026-05-29 20:09:05 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // 3. Copie o Project URL e a anon public key em Project Settings > API.
 // 4. Cole abaixo.
 
-window.SUPABASE_URL = "https://sdaxxrphbvxoertnofnw.supabase.co/rest/v1/";
+window.SUPABASE_URL = "https://sdaxxrphbvxoertnofnw.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_qB03yFr7MZg2va7FZZg0oA_wKf8rJHW";
